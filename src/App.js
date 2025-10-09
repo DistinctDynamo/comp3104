671150a7fc3057d5f580a2d95a3d682557ede6ad
Steven Nguyen <nguyensteven578@gmail.com>
2025-10-08 20:22:35 -0400

    Dep inclusions (#3)
    * Github action for github pages added
    
    * Name changed

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ const App = () => {
   return (
     <div className="App">
       <h1>Student Information System</h1>
-      <h5>Developed By: 1011122624 || Steven Nguyen || DevOps</h5>
+      <h5>Developed By: 1011122624 || Steven Nguyen || COMP3104DevOps</h5>
       <StudentForm addStudent={addStudent} />
       <StudentList students={students} />
     </div>
